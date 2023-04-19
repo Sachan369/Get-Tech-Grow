75dcfe07506e56ae1e66625ad0d5b41bbd20da09
Sachan369 <119755886+Sachan369@users.noreply.github.com>
2023-04-19 22:15:21 +0530

    Add files via upload

---
+++ project #2/main.js
@@ -0,0 +1,27 @@
+(function ($) {
+    "use strict";
+
+    // Spinner
+    var spinner = function () {
+        setTimeout(function () {
+            if ($('#spinner').length > 0) {
+                $('#spinner').removeClass('show');
+            }
+        }, 1);
+    };
+    spinner();
+    
+      // Sticky Navbar
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 45) {
+            $('.navbar').addClass('sticky-top shadow-sm');
+        } else {
+            $('.navbar').removeClass('sticky-top shadow-sm');
+        }
+    });
+    
+    
+
+    
+})(jQuery);
+
